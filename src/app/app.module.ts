662a3cd8b28c5aad0aee4162e368e02b1b1920a3
Klajdi <klajdihoxha146@gmail.com>
2023-05-03 18:16:57 +0200

    mesazh klajdi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    LoginComponent
+  ]
 })
-export class AppModule { }
+export class LoginModule { }
